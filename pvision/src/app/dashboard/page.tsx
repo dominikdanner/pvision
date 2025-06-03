4657e4c800e85a7ccc3db02d067f4daab06e8e9d
Severin Aichinger <40146720210188@litec.ac.at>
2025-06-03 13:19:53 +0200

    feat sidebar layout

--- pvision/src/app/dashboard/page.tsx
+++ pvision/src/app/dashboard/page.tsx
@@ -8,9 +8,11 @@ export default function Dashboard() {
   const state = useState<number>(0);
 
   return (
-    <div className="flex flex-row h-full">
-        <div className="w-1/3 bg-secondary">adsfasf</div>
-        <div className="w-2/3 bg-red-200"> gsgsgsgsg</div>
+    <div className="flex flex-row min-h-screen items-stretch">
+        <div className="w-1/3 bg-amber-300">
+            
+        </div>
+        <div className="w-2/3 bg-red-200"></div>
     </div>
   );
 }
